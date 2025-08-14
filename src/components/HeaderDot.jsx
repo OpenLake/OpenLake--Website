7fa729456f741c83a804015b466fb3df29439379
Chaithali <bodikechaithali@gmail.com>
2025-08-15 00:49:27 +0530

    NavigationButton

--- src/components/HeaderDot.jsx
+++ src/components/HeaderDot.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./css/HeaderDot.css";
 
 const HeaderDot = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(-1);
+    };
     return (
-        <div className="box">
+        <div className="box" onClick={handleClick} style={{ cursor: "pointer" }}>
             <div className="group-wrapper">
                 <div className="group">
                     <div className="overlap-group">
                         <div className="ellipse" />
                         <div className="div" />
                         <div className="ellipse-2" />
-                        <div className="ellipse-3" />
+                        <span className="arrow">&#8592;</span>
                     </div>
                 </div>
             </div>
