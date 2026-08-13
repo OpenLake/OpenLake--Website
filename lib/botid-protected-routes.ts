686f85b37baf85a976c17540c160fad6f8d8c6d2
ashish-kumar-dash <ashishdash2410@gmail.com>
2026-08-13 11:14:41 +0530

    chore(auth): remove login/admin stack; site is frontend-only

--- lib/botid-protected-routes.ts
+++
@@ -1,9 +0,0 @@
-export type BotIdProtectedRoute = {
-  path: string;
-  method: string;
-  advancedOptions?: {
-    checkLevel?: "basic" | "deepAnalysis";
-  };
-};
-
-export const botIdProtectedRoutes: BotIdProtectedRoute[] = [];
